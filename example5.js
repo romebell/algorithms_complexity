94b7c2087d0d850e92e88b61492d55e31b092bed
Rome Bell <romebell5@gmail.com>
2020-09-17 14:33:20 -0700

    [example5] Add code, Big O, and visual

---
+++ example5.js
@@ -0,0 +1,29 @@
+function example5(n) {
+    if (n <= 1) return;
+
+    example5(n - 1);
+    example5(n - 1);
+    example5(n - 1);
+    example5(n - 1);
+}
+
+// Example with 5
+// Viual on images/example5.jpg
+
+
+// Big O
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// O(4^n)
