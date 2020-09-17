ded5345bc56c724e0a450ca99c33a59da92a4cd7
Rome Bell <romebell5@gmail.com>
2020-09-17 13:17:20 -0700

    [factorial_problem] Add problem in py and js

---
+++ factoial_problem.py
@@ -0,0 +1,22 @@
+# Factorial 
+# DISCLAIMER: Not showing factorial solution, just an example of 
+# what a factorial problem would look like
+
+def factorial_problem(n):
+    if n == 1:
+        return
+    for i in range(1, n):
+        factorial_problem(n - 1)
+
+# The number of times that we branch is depended on size of input
+# ref Graph
+
+# Javascript
+# function factorialProblem(n) {
+#     if (n === 1) return;
+
+#     for (let i = 1; i <= n; i++) {
+#         factorialProblem(n - 1)
+#     }
+# }
+
