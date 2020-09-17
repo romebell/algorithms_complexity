c198605ca3b5f6d9cc55c0f20cea6467d5e57ebf
Rome Bell <romebell5@gmail.com>
2020-09-17 12:29:48 -0700

    [logarithmic] Add function in python and js

---
+++ logarithmic.py
@@ -0,0 +1,16 @@
+# Logarithmic - O(log(n))
+def logarithmic(n):
+    if n <= 1:
+        return
+    logarithmic(n / 2)
+
+# If a pass in 8, my input keeps dividing 4
+# We not really solving problems, but rather than getting a framework
+# of how the problems will look like
+
+
+# Javascript
+# function logarithmic(n) {
+#     if (n <= 1) return
+#     logarithmic(n / 2)
+# }
