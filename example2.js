ed743ad2d6cde61f1f37b3848b4f93e11d151222
Rome Bell <romebell5@gmail.com>
2020-09-17 14:31:58 -0700

    [example2] Add code and Big O

---
+++ example2.js
@@ -0,0 +1,28 @@
+function example2(array) {
+    for (let i = 0; i <= array.length; i++) {
+        for (let i = 0; i < array.length; i++) {
+            // do work
+        }
+    }
+
+    for(let k = 0; k < array.length; k++) {
+        // do more work
+    }
+}
+
+// Time Complexity ? 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// T(n^2 + n) =>  O(n^2)
