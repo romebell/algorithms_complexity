3e7b8e7dd29cf8f20a50be7af75da0c310681a40
Rome Bell <romebell5@gmail.com>
2020-09-17 12:40:00 -0700

    [linear] Add 2 examples in python and js

---
+++ linear.py
@@ -0,0 +1,31 @@
+# Linear - O(n)
+
+def linear_1(n):
+    for i in range(0, n):
+        print(i)
+
+# iterative linear function
+
+def linear_2(n):
+    if (n == 1): 
+        return
+    linear_2(n - 1)
+
+# pass in 10
+# 10 -> 9 -> 8 -> 7 -> 6 ...
+
+# JavaScript
+# function linear_1(n) {
+#     for (let i = 1; i <= n; i++) {
+#         # something
+#     }
+# }
+
+# function linear_2(n) {
+#     if (n === 1) {
+#         return;
+#     }
+
+#     linear_2(n - 1);
+# }
+
