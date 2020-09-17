d7b3b42ab18f583941bc28049942be6ba58566c9
Rome Bell <romebell5@gmail.com>
2020-09-17 14:24:58 -0700

    [example1] Add code and answer

---
+++ example1.js
@@ -0,0 +1,28 @@
+function example1(array) {
+    for (let i = 0; i <= 20; i++) {
+        for (let i = 0; i < array.length; i++) {
+            // do something
+        }
+    }
+}
+
+// Time Complexity ? 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// T (20n) => O(n)
---
+++ example6.js
@@ -0,0 +1,31 @@
+function example6(n) {
+    if (n <= 1) return;
+    for (let i = 1; i <= 4; i++) {
+        example6(n - 1)
+    }
+}
+
+// Example with 5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// notice similar, if not the same as example5.js
+// O(4n)
