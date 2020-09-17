ea97daed9c716fc1815795569d5547f6c05d74d1
Rome Bell <romebell5@gmail.com>
2020-09-17 14:32:41 -0700

    [example4] Add code and Big O

---
+++ example4.js
@@ -0,0 +1,46 @@
+function example4(n) {
+    if (n === 0) return;
+
+    for (let i = 1; i <= 23; i++) {
+        // let's go!!!
+    }
+
+    example4(n - 1)
+}
+
+// Example with 5 
+// Recursive chain
+/*
+5
+|
+4
+|
+3
+|
+2
+|
+1
+|
+0
+*/
+
+// For every single recursive call, we have a loop
+
+
+
+
+
+
+// T(23n) -> O(n)
+
+
+
+
+
+
+
+
+
+
+
+
