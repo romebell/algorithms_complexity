ada7f4d73eeca125f5604bb03aafa4b176627c61
Rome Bell <romebell5@gmail.com>
2020-09-17 13:01:19 -0700

    [polynomial] Add quadratic and cubic examples in py and js

---
+++ polynomial.py
@@ -0,0 +1,36 @@
+# Polynomial - O(n^2)
+
+def quadratic(n):
+    for i in range(1, n):
+        for j in range(1, n):
+                pass
+
+# Polynomial - O(n^3)
+def cubic(n):
+    for i in range(1, n):
+        for j in range(1, n):
+            for k in range(1, n):
+                pass
+
+# we have to look for nested loops where both depend on the size of our input
+
+
+# function quadratic(n) {
+#     for (let i = 1; i <= n; i++) {
+#         for (let j = 1; j <= n; j++) {
+#             # something
+#         }
+#     }
+# }
+
+
+# Polynomial - O(n^3)
+# function cubic(n) {
+#     for (let i = 1; i <= n; i++) {
+#             for (let j = 1; j <= n; j++) {
+#                 for (let k = 1; k <= n; k++) {
+#                     # something
+#                 }
+#             }
+#         }
+# }
