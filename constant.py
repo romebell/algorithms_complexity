ae6fd19f9deff5490d674462e3dd29c5843f9808
Rome Bell <romebell5@gmail.com>
2023-07-25 20:24:45 -0700

    Update constant.py

--- constant.py
+++ constant.py
@@ -10,13 +10,15 @@ def constant_2(n):
 # T(100) => O(1)
 
 
-# Javascript
-# function constant_1(n) {
-#     return n * 2 + 1
-# }
+```js
+function constant_2(n) {
+    for (let i = 1; i <= 100, i++) {
+      console.log(i)
+    }
+}
 
-# function constant_2(n) {
-#     for (let i = 1; i <= 100, i++) {
-#       console.log(i)
-#     }
-# }
+function constant_1(n) {
+   return n * 2 + 1
+}
+
+```
